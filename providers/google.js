86e2f507cfff12230cda6e6544ac800379e01b08
Brendan Jennings <jbrendan70@outlook.com>
2021-01-10 22:06:22 +0000

    🛂 Lazy tired work on callback (Google)

--- providers/google.js
+++ providers/google.js
@@ -6,6 +6,7 @@
          * 3. Send the access token to an API
          * 
          */
+const { config } = require('grunt');
 const fetch = require('node-fetch');
 
 class GoogleHandler {
@@ -15,26 +16,33 @@ class GoogleHandler {
         this.client_secret = configobj.client_secret;
 
         this.redirect_uri = configobj.redirect_uri; //We recommend settings this to something like https://example.org/api/oauth2/google
-        this.scope = configobj.scope >= 1 ? configobj.scope : "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile";//Default to profile scope if no scope is defined    -  && configobj.isArray()
+        this.scope = configobj.scope >= 1 ? configobj.scope : "https://www.googleapis.com/auth/userinfo.email+https://www.googleapis.com/auth/userinfo.profile";//Default to profile scope if no scope is defined    -  && configobj.isArray()
         this.auth_base_url = "https://accounts.google.com/o/oauth2/v2/auth"
-        this.token_url = "https://www.googleapis.com/oauth2/v4/token"
+        this.token_url = "https://oauth2.googleapis.com/token"
+
     }
 
     startFlow() {//Should return a uri to begin the OAuth2 flow and gain user consent
         const body = {};
-        fetch(`${this.auth_base_url}?client_id=${this.client_id}&response_type=token&scope=${this.scope}&redirect_uri=${this.redirect_uri}/callback`,
-            {
-                method: 'GET',
-            })
+
+        fetch(`${this.auth_base_url}?client_id=${this.client_id}&response_type=token&scope=${this.scope[1]}&redirect_uri=${this.redirect_uri}/callback`, {method: 'GET'})
             .then(res => res.json())
             .then(json => { try { return (JSON.parse(json)) } catch (e) {throw "bad response"} })
         console.info('googleflow')
+        
+        // Get uri for auth
     }
 
     stopFlow(flowResponse) {//Should receive the token, automatically and prepare it for the user - the token is not stored and this should return USER DATA only
-        if (!flowResponse)
+        if (!flowResponse || !flowResponse.code)
             return false
         console.info('googlecallback')
+
+        fetch(`${this.token_url}?code=${flowResponse.code}`, {method:'POST'})// Get user code from query data -> ${flowResponse.code}
+            .this(res => res.json())
+            .this(json => {})// Get user token -> function fetch ...
+        
+        // Get user data (email, name)
     }
 
     renewToken(token) {//Should renew the token
