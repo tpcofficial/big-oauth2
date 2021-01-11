c1b1cafc04acdfdc3843a7c34e7edf5aae432c5b
Brendan Jennings <jbrendan70@outlook.com>
2021-01-11 14:03:37 +0000

    🐛 Comment out generic so it doesn't cause issues

--- providers/generic.js
+++ providers/generic.js
@@ -1,18 +1,18 @@
 
-{
-    "client_id": '',
-    "client_secret": '',
-    "redirect_uri": '',
-    "scopes":"",
+// {
+//     "client_id": '',
+//     "client_secret": '',
+//     "redirect_uri": '',
+//     "scopes":"",
 
-    "authentication_uri":'',
-    "platform_name":'',
-    "token_uri":'',
-    "":""
-}
+//     "authentication_uri":'',
+//     "platform_name":'',
+//     "token_uri":'',
+//     "":""
+// }
 
-// Query for access code
+// // Query for access code
 
-// Query for access token
+// // Query for access token
 
-// Query for data (may differ across platforms)
+// // Query for data (may differ across platforms)
